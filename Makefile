1bdd9ca29163f4a158830d61a0266b5096010d10
root <root@pi4.yhpark>
2020-06-02 01:21:18 +0000

    make & resource file added

---
+++ Makefile
@@ -0,0 +1,2 @@
+web_client: web_client.c
+	gcc -o web_client web_client.c -I. -lcurl
--- web_client.c
+++ web_client.c
@@ -2,53 +2,53 @@
 
 int main(void)
 {
-        CURL *easy = curl_easy_init();
-        curl_mime *mime;
-        curl_mimepart *part;
+	CURL *easy = curl_easy_init();
+	curl_mime *mime;
+	curl_mimepart *part;
 
-        /* Build an HTTP form with a single field named "data", */
-        mime = curl_mime_init(easy);
+	/* Build an HTTP form with a single field named "data", */
+	mime = curl_mime_init(easy);
 
-        part = curl_mime_addpart(mime);
-        curl_mime_name(part, "first_name");
-        curl_mime_data(part, "youngho", CURL_ZERO_TERMINATED);
+	part = curl_mime_addpart(mime);
+	curl_mime_name(part, "first_name");
+	curl_mime_data(part, "youngho", CURL_ZERO_TERMINATED);
 
-        part = curl_mime_addpart(mime);
-        curl_mime_name(part, "last_name");
-        curl_mime_data(part, "park", CURL_ZERO_TERMINATED);
+	part = curl_mime_addpart(mime);
+	curl_mime_name(part, "last_name");
+	curl_mime_data(part, "park", CURL_ZERO_TERMINATED);
 
-        part = curl_mime_addpart(mime);
-        curl_mime_name(part, "position");
-        curl_mime_data(part, "center backc", CURL_ZERO_TERMINATED);
+	part = curl_mime_addpart(mime);
+	curl_mime_name(part, "position");
+	curl_mime_data(part, "center backc", CURL_ZERO_TERMINATED);
 
-        part = curl_mime_addpart(mime);
-        curl_mime_name(part, "number");
-        curl_mime_data(part, "17", CURL_ZERO_TERMINATED);
+	part = curl_mime_addpart(mime);
+	curl_mime_name(part, "number");
+	curl_mime_data(part, "17", CURL_ZERO_TERMINATED);
 
-        part = curl_mime_addpart(mime);
-        curl_mime_name(part, "username");
-        curl_mime_data(part, "user2", CURL_ZERO_TERMINATED);
+	part = curl_mime_addpart(mime);
+	curl_mime_name(part, "username");
+	curl_mime_data(part, "user2", CURL_ZERO_TERMINATED);
 
-        part = curl_mime_addpart(mime);
-        curl_mime_name(part, "image");
-        curl_mime_filename(part, "./1.png");
+	part = curl_mime_addpart(mime);
+	curl_mime_name(part, "image");
+	curl_mime_filename(part, "image.png");
 
-        {
-                FILE *fp = fopen("1.png", "r");
-                char buf[8192];
-                int len = fread(buf, 1, sizeof(buf), fp);
-                printf("len : %d\n", len);
-                fclose(fp);
+	{
+		FILE *fp = fopen("image.png", "r");
+		char buf[8192];
+		int len = fread(buf, 1, sizeof(buf), fp);
+		printf("len : %d\n", len);
+		fclose(fp);
 
-                curl_mime_data(part, buf, len);
-        }
+		curl_mime_data(part, buf, len);
+	}
 
-        /* Post and send it. */
-        curl_easy_setopt(easy, CURLOPT_MIMEPOST, mime);
-        curl_easy_setopt(easy, CURLOPT_URL, "http://192.168.3.222:2000/add");
-        curl_easy_perform(easy);
+	/* Post and send it. */
+	curl_easy_setopt(easy, CURLOPT_MIMEPOST, mime);
+	curl_easy_setopt(easy, CURLOPT_URL, "http://192.168.3.222:2000/add");
+	curl_easy_perform(easy);
 
-        /* Clean-up. */
-        curl_easy_cleanup(easy);
-        curl_mime_free(mime);
+	/* Clean-up. */
+	curl_easy_cleanup(easy);
+	curl_mime_free(mime);
 }
